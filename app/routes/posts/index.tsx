bd9078dd8c02401a4bbc0c725611f64171e9bb9b
Andres2D <andres2d1997.1206@gmail.com>
2023-04-13 17:08:05 -0500

    Use loader to fetch data from server

--- app/routes/posts/index.tsx
+++ app/routes/posts/index.tsx
@@ -1,7 +1,38 @@
+import { Link, useLoaderData } from '@remix-run/react';
+import { json } from '@remix-run/node';
+import type { Post } from '../../interfaces/post';
+
+export const loader = async () => {
+  const posts: Post[] = [
+    {
+      slug: 'how-to-became-frontend-developer',
+      title: 'How to became a frontend developer'
+    },
+    {
+      slug: 'create-your-first-portfolio-page',
+      title: 'Create your first portfolio page'
+    },
+  ];
+  
+  return json({posts})
+};
+
 const PostsRoutes = () => {
+
+  const { posts } = useLoaderData();
+
   return ( 
     <main>
       <h1>Posts</h1>
+      <ul>
+        {posts.map((post: Post) => (
+          <li key={post.slug}>
+            <Link to={post.slug} className="text-blue-600 underline">
+              {post.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </main>
   );
 };
