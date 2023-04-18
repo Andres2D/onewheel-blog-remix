0f63423f739e982fa4c3c38368a6390a6456a0e7
Andres2D <andres2d1997.1206@gmail.com>
2023-04-18 10:48:35 -0500

    Improve typing on server

--- app/routes/posts.admin.new.tsx
+++ app/routes/posts.admin.new.tsx
@@ -1,7 +1,14 @@
 import { Form, useActionData } from "@remix-run/react";
 import { type ActionFunction, redirect, json } from "@remix-run/node";
 import { createPost } from "~/models/post.server";
+import invariant from 'tiny-invariant';
 const inputClassName = `w-full rounded border border-gray-500 px-2 py-1 text-lg`;
+
+type ActionData = {
+  title: null | string,
+  slug: null | string,
+  markdown: null | string
+} | undefined;
 
 export const action: ActionFunction = async ({ request }) => {
   const formData = await request.formData(); 
@@ -9,7 +16,7 @@ export const action: ActionFunction = async ({ request }) => {
   const slug = formData.get('slug');
   const markdown = formData.get('markdown');
 
-  const errors = {
+  const errors: ActionData = {
     title: title ? null : 'Title is required',
     slug: slug ? null : 'Slug  is required',
     markdown: markdown ? null : 'Markdown is required',
@@ -17,8 +24,12 @@ export const action: ActionFunction = async ({ request }) => {
   const hasErrors = Object.values(errors).some(errorMessage => errorMessage);
 
   if(hasErrors) {
-    return json(errors)
+    return json<ActionData>(errors)
   }
+
+  invariant(typeof title === 'string', 'Title must be a string');
+  invariant(typeof slug === 'string', 'Slug must be a string');
+  invariant(typeof markdown === 'string', 'Markdown must be a string');
 
   await createPost({title, slug, markdown});
 
@@ -26,7 +37,7 @@ export const action: ActionFunction = async ({ request }) => {
 };
 
 export default function AdminIndexRoute() {
-  const errors = useActionData();
+  const errors = useActionData() as ActionData;
   return (
     <Form method="post">
       <p>
